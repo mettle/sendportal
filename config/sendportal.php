8db761693738a30f62a27bc8a1ca2d1b671a9e36
David Field <david@mettle.io>
2020-05-27 11:09:59 +0100

    wip

--- config/sendportal.php
+++ config/sendportal.php
@@ -28,4 +28,15 @@ return [
         'register' => env('SENDPORTAL_REGISTER', false),
         'password_reset' => env('SENDPORTAL_PASSWORD_RESET', true),
     ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | API Throttle Settings
+    |--------------------------------------------------------------------------
+    |
+    | Configure the Sendportal API throttling.
+    | For more information see https://laravel.com/docs/master/routing#rate-limiting
+    |
+    */
+    'throttle_middleware' => 'throttle:' . env('SENDPORTAL_THROTTLE_MIDDLEWARE', '60,1'),
 ];
