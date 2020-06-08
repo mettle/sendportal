7291f2118f328e1740f2e92082966d9db5bc6aec
David Field <david@mettle.io>
2020-06-08 08:40:34 +0100

    wip

--- routes/console.php
+++ routes/console.php
@@ -13,7 +13,3 @@ use Illuminate\Support\Facades\Artisan;
 | simple approach to interacting with each command's IO methods.
 |
 */
-
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->describe('Display an inspiring quote');
