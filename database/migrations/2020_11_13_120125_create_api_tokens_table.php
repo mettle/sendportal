008d0a411cf08e908c32c287d5d806f706b3e7ff
Aivis Silins <aivis.silins@gmail.com>
2020-11-13 12:20:51 +0000

    Add api_tokens table

---
+++ database/migrations/2020_11_13_120125_create_api_tokens_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateApiTokensTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('api_tokens', function (Blueprint $table) {
+            $table->id();
+            $table->string('api_token');
+            $table->unsignedInteger('workspace_id')->index();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('api_tokens');
+    }
+}
