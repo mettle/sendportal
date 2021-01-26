8a2354600c5b3d8d60f51c1632efd24a9ecf3967
David Field <david@mettle.io>
2021-01-26 15:13:02 +0000

    Fix some small issues
    - API endpoints now back under /api
    - User model correctly set in auth config
    - Removed great big TODO because it isn't TODO

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -74,7 +74,8 @@ class RouteServiceProvider extends ServiceProvider
      */
     protected function mapApiRoutes()
     {
-        Route::middleware('api')
+        Route::prefix('api')
+            ->middleware('api')
             ->namespace($this->namespace)
             ->group(base_path('routes/api.php'));
     }
