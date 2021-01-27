06ef284e7b3029e1d88c72bb42a9508a2a1217b6
mauricius <maurizio.bonani@gmail.com>
2021-01-27 13:53:46 +0100

    Update menu class

--- resources/views/layouts/sidebar/manageUsersMenuItem.blade.php
+++ resources/views/layouts/sidebar/manageUsersMenuItem.blade.php
@@ -2,7 +2,7 @@
     @if (auth()->user()->ownsCurrentWorkspace())
         <li class="nav-item {{ request()->is('users*') ? 'active' : '' }}">
             <a class="nav-link" href="{{ route('users.index') }}">
-                <i class="fas fa-users mr-2"></i><span>{{ __('Manage Users') }}</span>
+                <i class="fa-fw fas fa-users mr-2"></i><span>{{ __('Manage Users') }}</span>
             </a>
         </li>
     @endif
