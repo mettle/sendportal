89bfa34f199899ea127ee1cd1b8012fc206c69bf
Maurizio <maurizio.bonani@gmail.com>
2021-03-08 11:36:30 +0100

    Remove useless rules

--- app/Http/Requests/ProfileUpdateRequest.php
+++ app/Http/Requests/ProfileUpdateRequest.php
@@ -28,10 +28,6 @@ class ProfileUpdateRequest extends FormRequest
             'locale' => [
                 'required',
                 Rule::in(array_keys(config()->get('sendportal-host.locale.supported')))
-            ],
-            'password_confirmation' => [
-                'required_with:password',
-                'confirmed'
             ]
         ];
     }
