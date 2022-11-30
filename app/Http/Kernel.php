0699fe053e489105f7d5383b8c37107c190df26c
Pushkraj Jori <pushkrajjori19@gmail.com>
2022-11-30 12:54:40 +0530

    API and Pagination issues fixed
    Dynamic Rate limiting from route middleware was not working cause of default throttle so its commented out now.
    
    Pagination css was not working so its fixed now.

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -40,7 +40,7 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
-            'throttle:60,1',
+            // 'throttle:60,1',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
     ];
